249f6a5927c26817877baa839f5e2ab823bd5b50
DamianLaczynski <damian.laczynski.edu@gmail.com>
2022-07-03 12:13:05 +0200

    Dodaj pliki projektów.

---
+++ Production Management/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Production_Management
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void signInPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            signOrRegisterFrame.Content = new SignInPage();
+
+            GradientStopCollection gradientStops = new GradientStopCollection();
+            gradientStops.Add(new GradientStop((Color)ColorConverter.ConvertFromString("#FF5EB3B9"), 1));
+            gradientStops.Add(new GradientStop((Color)ColorConverter.ConvertFromString("#FF0815DE"), 0));
+            LinearGradientBrush linearGradientBrush = new LinearGradientBrush();
+            linearGradientBrush.StartPoint = new Point(0.5, 0);
+            linearGradientBrush.EndPoint = new Point(0.5, 1);
+            linearGradientBrush.GradientStops = gradientStops;
+            registerPageButton.Background = new SolidColorBrush((Color)Colors.Transparent);
+            signInPageButton.Background = linearGradientBrush;
+        }
+
+        private void registerPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            signOrRegisterFrame.Content = new RegisterPage();
+
+            GradientStopCollection gradientStops = new GradientStopCollection();
+            gradientStops.Add(new GradientStop((Color)ColorConverter.ConvertFromString("#FF5EB3B9"), 1));
+            gradientStops.Add(new GradientStop((Color)ColorConverter.ConvertFromString("#FF0815DE"), 0));
+            LinearGradientBrush linearGradientBrush = new LinearGradientBrush();
+            linearGradientBrush.StartPoint = new Point(0.5, 0);
+            linearGradientBrush.EndPoint = new Point(0.5, 1);
+            linearGradientBrush.GradientStops = gradientStops;
+            registerPageButton.Background = linearGradientBrush;
+            signInPageButton.Background = new SolidColorBrush((Color)Colors.Transparent);
+        }
+    }
+}
---
+++ Production Management/SignInPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Production_Management
+{
+    /// <summary>
+    /// Logika interakcji dla klasy SignInPage.xaml
+    /// </summary>
+    public partial class SignInPage : Page
+    {
+        public SignInPage()
+        {
+            InitializeComponent();
+        }
+
+        private void signInButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
